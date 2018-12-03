317a5679a5980c1978eabbb906863575aea35f50
Philipp137 <philipp.l.krah@gmail.com>
2018-12-03 15:02:25 +0100

    some updates

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -10,11 +10,15 @@ Created on Fri Nov 30 09:11:12 2018
 # IMPORTED MODULES
 ###############################################################################
 import numpy as np
-from numpy import exp
+from numpy import exp, mod
 import matplotlib.pyplot as plt
 from sPOD_tools import frame,sPOD
 ###############################################################################
 
+##########################################
+#%% Define your DATA:
+##########################################
+plt.close("all")
 Nx = 200 # number of grid points in x 
 Nt = 250 # numer of time intervalls
 dt = 0.01 # size of time intervall
@@ -22,26 +26,52 @@ T = Nt*dt # total time
 L = 2*np.pi  # total domain size
 x0 = L*0.5 # starting point of the gauss puls
 sigma=L/50 # standard diviation of the puls
-c = 1.3*L/T # speed of the traveling wave
 
-nmodes = 2 # reduction of singular values
+nmodes = 1 # reduction of singular values
 x = np.linspace(0,L,Nx)
 t = np.linspace(0,T,Nt)
 dx=x[1]-x[0]
 dt=t[1]-t[0]
 c=dx/dt
-[X,T] =meshgrid(x,t)
-fun = lambda x,t: exp( -(mod((x-c*t),L)-x0)**2/sigma**2 *0.5 ) + \
-                  exp( -(mod((x+c*t),L)-x0)**2/sigma**2 *0.5)
+[T,X] =meshgrid(t,x)
+fun = lambda x,t: 0.5 * exp( -(mod((x-c*t),L)-x0)**2/sigma**2 ) + \
+                  0.5 * exp( -(mod((x+c*t),L)-x0)**2/sigma**2 )
 
-field = [fun(X, T)]
+# Define your field as a list of fields:
+# For example the first element in the list can be the density of 
+# a flow quantity and the second element could be the velocity in 1D
+density = fun(X, T)
+velocity = fun(X, T)
+fields = [density]#, velocity]
 
 
-plotten=True
-eps=1e-4
-rel_err=1
+#######################################
+#%% CALL THE SPOD algorithm
+######################################
+sPOD_frames = sPOD(fields, [-c,c], dx, dt, nmodes=1, eps=1e-4, Niter=30, visualize=False)
 
-sPOD(field, [-c,c], dx, dt, nmodes=2, eps=1e-4, Niter=5, visualize=False)
+#####################################
+#%% visualize your results:
+#####################################
+# the result is a list of the decomposed field.
+# each element of the list contains a frame of the decomposition.
+# If you want to plot the k-th frame use: 
+## 1. frame
+k_frame = 0
+plt.suptitle("sPOD Frames")
+plt.subplot(121)
+sPOD_frames[k_frame].plot_field()
+plt.xlabel(r'$N_x$')
+plt.ylabel(r'$N_t$')
+plt.title(r"$q^"+str(k_frame)+"(x,t)$")
+## 2. frame
+k_frame = 1
+plt.subplot(122)
+sPOD_frames[k_frame].plot_field()
+plt.xlabel(r'$N_x$')
+plt.ylabel(r'$N_t$')
+plt.title(r"$q^"+str(k_frame)+"(x,t)$")
+# by default this will plot the field in the first component 
+# of your field list (here: density)
 
-#f_frame=frame(c, dx, dt, field, nmodes)
-    
+ 
--- lib/sPOD_tools.py
+++ lib/sPOD_tools.py
@@ -63,13 +63,35 @@ class frame:
         frame. This can be done by build_field().
         
         """
-        c=self.velocity
+        c = self.velocity
         idx_shift = c*self.dt/self.dx
-        Ix = range(size(field, 1))
+        Ix = range(np.size(field,0))
         qshift = field.copy()
-        for j in range(size(field,0)):
+        for j in range(self.Ntime):
              #roll(qshift[j,:],int(idx_shift)*j)
-            qshift[j,:] = interp(Ix-idx_shift*j, Ix, qshift[j,:], period=Ix[-1])
+            qshift[:,j] = interp(Ix-idx_shift*j, Ix, qshift[:,j], period=Ix[-1])
+        
+        return qshift
+
+    def inv_shift(self, field):
+        """
+        This function returns the shifted field.
+        $q(x-s,t)=T^s[q(x,t)]$ 
+        here the shift is simply s=c*t
+        In the default case where c= ~velocity of the frame~,
+        the field is shifted back to the original frame. 
+        ( You may call it the labratory frame)
+        Before we shift the frame has to be put togehter in the co-moving
+        frame. This can be done by build_field().
+        
+        """
+        c = -self.velocity
+        idx_shift = c*self.dt/self.dx
+        Ix = range(np.size(field,0))
+        qshift = field.copy()
+        for j in range(self.Ntime):
+             #roll(qshift[j,:],int(idx_shift)*j)
+            qshift[:,j] = interp(Ix-idx_shift*j, Ix, qshift[:,j], period=Ix[-1])
         
         return qshift
 
@@ -89,9 +111,10 @@ class frame:
         
     def set_orhonormal_system(self, dx, dt, fields):
         """
-         
+        In this routine we set the orthonormal vectors of the SVD in the 
+        corresponding frames.
         """
-        
+
         # spatial lattice spacing
         self.dx = dx
         # timestep
@@ -103,65 +126,90 @@ class frame:
         # number of space dimensions
         self.dim = fields[0].ndim - 1
         # list of spatial points in each dimension
-        self.Nspace = np.ones(2)
+        self.Nspace = np.ones(2,dtype=int)
         self.Nspace[:self.dim] = fields[0].shape[:self.dim]
         # number of timesteps
         self.Ntime = fields[0].shape[-1]
-
+        # number elements in each row of the snapshot matrix
+        self.Nspace_var= np.prod(self.Nspace)*self.Nfields
         # init field
         X = []
 
         # loop
-        for idx_field,field in enumerate(fields):
+        for idx_field, field in enumerate(fields):
             field_shift = self.shift(field)
             field_shift = reshape(field_shift, [-1, self.Ntime])
             X.append(field_shift)
-            
-        [U, S, VT] = self.reduce(X[0])
-        self.modal_system = {"U" : U, "sigma" : S, "VT": VT}
-  
+        
+        # in this step we produce the snappshot matrix X_{i,j}
+        # i --- labels the spatial points in every component
+        #       so if Nspace=20 and Nfields=3 then i runs
+        #       from 0 to 20*3-1
+        # j --- labels the temporal points only
+        X = np.concatenate(X) 
+        # make an singular value decomposition of the snapshot matrix
+        # and reduce it to the specified numer of moddes
+        [U, S, VT] = self.reduce(X)
+        # the snapshot matrix is only stored with reduced number of SVD modes
+        self.modal_system = {"U": U, "sigma": S, "VT": VT}
+
     def build_field(self):
+        """
+        Calculate the field from the SVD modes: X=U*S*VT
+        """
         # modes from the singular value decomposition
         u = self.modal_system["U"]
         s = self.modal_system["sigma"]
-        vh= self.modal_system["VT"]
+        vh = self.modal_system["VT"]
         # add up all the modes A=U * S * VH
-        return np.reshape(np.dot(u * s, vh),self.field_shape)
-    
-    def plot_field(self,field_index=0):
+        return np.reshape(np.dot(u * s, vh), self.field_shape)
+
+    def plot_field(self, field_index=0):
+
+        if self.dim == 1:
+            snapshot = self.inv_shift(self.build_field()[field_index])
+            # the snapshot matrix is transposed 
+            # in order to have the time on the y axis
+            pcolor(snapshot.T)
+            #plt.xlabel(r"$N_x$")
+            #plt.ylabel(r"$N_t$")
+
+        if self.dim == 2:
+            # TODO implement me
+            print("no plot function implemented")
         
-        pcolor(self.shift(self.build_field()[field_index]))
 
     def plot_singular_values(self):
         """
         This function plots the singular values of the frame.
         """
-        sigmas=self.modal_system["sigma"]        
-        semilogy(sigmas,"r+")
-        xlabel("i")
-        ylabel("$\sigma_i$")
-        
+        sigmas = self.modal_system["sigma"]
+        semilogy(sigmas, "r+")
+        xlabel(r"$i$")
+        ylabel(r"$\sigma_i$")
+
     def __add__(self, other):
         """ Add two frames for the purpose of concatenating there modes """
         # TODO make check if other and self can be added:
         # are they in the same frame? Are they from the same data etc.
-        new=frame(self.velocity,self.dx,self.dt,self.build_field(),self.Nmodes)
+        new = frame(self.velocity, self.dx, self.dt,
+                    self.build_field(), self.Nmodes)
         # combine left singular vecotrs
         Uself = self.modal_system["U"]
         Uother = other.modal_system["U"]
-        new.modal_system["U"]=np.concatenate([Uself,Uother],axis=1)
+        new.modal_system["U"] = np.concatenate([Uself, Uother], axis=1)
 
         # combine right singular vecotrs
         VTself = self.modal_system["VT"]
         VTother = other.modal_system["VT"]
-        new.modal_system["VT"]=np.concatenate([VTself,VTother],axis=0)
+        new.modal_system["VT"] = np.concatenate([VTself, VTother], axis=0)
 
-        Sself=self.modal_system["sigma"]
-        Sother=other.modal_system["sigma"]
-        new.modal_system["sigma"]=np.concatenate([Sself,Sother])
+        Sself = self.modal_system["sigma"]
+        Sother = other.modal_system["sigma"]
+        new.modal_system["sigma"] = np.concatenate([Sself, Sother])
 
-        new.Nmodes += other.Nmodes 
-        
+        new.Nmodes += other.Nmodes
+
         return new
 
 
@@ -170,53 +218,66 @@ class frame:
 # least square minimization
 ###############################################################################
 
-def minimize(Xtilde_frames,X):
-  ######################################################
+def minimize(Xtilde_frames, X):
+    ######################################################
     # build coef matrix X_coef_mat of eq. (9) in Reiss2017
     ######################################################
-    X_coef=[]       # lab frame
-    X_coef_shift=[] # co-moving frame
-    dx=Xtilde_frames[0].dx
-    dt=Xtilde_frames[0].dt
-    # number of moving frames
-    Nframes = len(Xtilde_frames)
+    X_coef = []        # lab frame
+    X_coef_shift = []  # co-moving frame
     # loop through all moving frames
-    for k,frame in enumerate(Xtilde_frames):
+    for k, frame in enumerate(Xtilde_frames):
         # left singular vectors
-        U=frame.modal_system["U"]
-        # right singular vectors 
-        VT=frame.modal_system["VT"]
+        U = frame.modal_system["U"]
+        # right singular vectors
+        VT = frame.modal_system["VT"]
         # singular values
-        sigmas=frame.modal_system["sigma"]
-        Nmodes=frame.Nmodes
-        # velocity in the corresponding frame
-        c = frame.velocity
-        
+        Nmodes = frame.Nmodes
         # loop over all the modes
         # each mode represents one alpha_k and one column of the matrix X
         for i in range(Nmodes):
-            Xmode_shift = np.outer(U[:,i],VT[i,:])    
-            Xmode = frame.shift(Xmode_shift)
-            
-            X_coef_shift.append(reshape(Xmode_shift,[-1]))
-            X_coef.append(reshape(Xmode,[-1]))
+            Xmode_shift = np.outer(U[:, i], VT[i, :])
+            Xmode = frame.inv_shift(Xmode_shift)
+
+            X_coef_shift.append(reshape(Xmode_shift, [-1]))
+            X_coef.append(reshape(Xmode, [-1]))
     ######################################################
-    # solve the minimication problem 
+    # solve the minimication problem
     ######################################################
+    # first we convert the list of vectors (reshaped matrices) to an array.
+    # We have to transpose it since  the function asarray
+    # concatenates the list vectors verticaly
     X_coef = asarray(X_coef).T
-    X_coef_shift = asarray(X_coef_shift)       
+    X_coef_shift = asarray(X_coef_shift).T
     X_ref = reshape(X.copy(), [-1, 1])
     # X_coef * alpha = X
     alpha = lstsq(X_coef, X_ref)[0]
     alpha = asarray(alpha)
-    
 
-    for k,frame in enumerate(Xtilde_frames):
-        X_frame = X_coef_shift.T[:,k*Nmodes:(k+1)*Nmodes] @ alpha[k*Nmodes:(k+1)*Nmodes] 
-        X_frame = reshape(X_frame, np.shape(X[0]))
-        [U, S, VT]=frame.reduce( X_frame )
-        frame.modal_system = {"U" : U, "sigma" : S, "VT": VT}
+    return alpha, X_coef_shift
 
+
+###############################################################################
+# update the Xtilde frames and truncate modes
+###############################################################################
+
+def update_and_reduce_modes(Xtilde_frames, alpha, X_coef_shift, Nmodes_reduce):
+
+    """
+    This function implements the 5. step of the SPOD algorithm (see Reiss2017)
+    - calculate the new modes from the optimiced alpha combining 
+    Xtilde and R modes.
+    - truncate the number of modes to the desired number of reduced modes 
+    - update the new Xtilde in the corresponding frames
+    """
+    for k, frame in enumerate(Xtilde_frames):
+        Nmodes = frame.Nmodes
+        alpha_k = alpha[k*Nmodes:(k+1)*Nmodes]
+        # linear combination to get the new Xtilde
+        Xnew_k = X_coef_shift[:, k*Nmodes:(k+1)*Nmodes] @ alpha_k
+        Xnew_k = reshape(Xnew_k, [-1, frame.Ntime])
+        frame.Nmodes = Nmodes_reduce  # reduce to the desired number of modes
+        [U, S, VT] = frame.reduce(Xnew_k)
+        frame.modal_system = {"U": U, "sigma": S, "VT": VT}
 
 # %%
 ###############################################################################
@@ -225,93 +286,98 @@ def minimize(Xtilde_frames,X):
 
 
 def sPOD(X, velocities, dx, dt, nmodes=2, eps=1e-4, Niter=5, visualize=True):
-    
+
     # plot the first component of the original field
     if visualize:
         subplot(1, 4, 1)
-        p = pcolor(X[0])
+        p = pcolor(X[0].T)
         plt.title(r'$X$')
         plt.ylabel(r"$N_t$")
         plt.xlabel(r"$N_x$")
         plt.pause(0.05)
         clims = p.get_clim()
-        
+
     #################################
     # 1. reset loop variables
     ################################
-    
-    Xtilde=np.zeros(np.shape(X))
-    Xtilde_frames=[frame(v,dx,dt,Xtilde,nmodes) for v in velocities]
-    rel_err=1
-    it=0
-    
+
+    Xtilde = np.zeros(np.shape(X))
+    Xtilde_frames = [frame(v, dx, dt, Xtilde, nmodes) for v in velocities]
+    rel_err = 1
+    it = 0
+
     ###########################################################################
     # MAIN LOOP
     ###########################################################################
     # loop until the desired precission is achieved or the maximal number
     # of iterations
-    while rel_err > eps and it < Niter :
-        
-        it+=1 # counts the number of iterations in the loop
+    while rel_err > eps and it < Niter:
 
-        
+        it += 1  # counts the number of iterations in the loop
+
         ###############################
         # 2. calculate residual R
         ###############################
         R = X - Xtilde
-        rel_err = norm(R)/norm(X) # relative error
+        rel_err = norm(R)/norm(X)  # relative error
 
         ##########################################
         print("iter= ", it, "rel err= ", rel_err)
         ##########################################
-        
-        if visualize: # plot the residual
-            subplot(1,len(Xtilde_frames)+2,2)
-            pcolor(R[0])   
-            plt.title(r"$R=X-\tilde{X}$")    
-            #plt.xlabel(r"$N_x$")
-            plt.pause(0.05)
 
         ###############################
         # 3. Multi shift and reduce R
         ###############################
-        R_frames=[frame(v, dx, dt, R, nmodes) for v in velocities]
-    
+        R_frames = [frame(v, dx, dt, R, nmodes) for v in velocities]
+
+        if visualize:  # plot the residual
+            subplot(1, len(Xtilde_frames)+2, 2)
+            pcolor(R[0].T)
+            plt.title(r"$R=X-\tilde{X}$")
+            # plt.xlabel(r"$N_x$")
+            plt.pause(0.05)
+
+        #################################
+        # 4. optimize with least squares
+        #################################
         #######################################################################
-        # 4. combine the modes of Xtilde and R
-        # Note 2 objects in the same frame, can be added by concatenating 
+        # a) combine the modes of Xtilde and R
+        # Note 2 objects in the same frame, can be added by concatenating
         # the SVD left and right singular vectors.
         #######################################################################
-        #Xtilde_frames = []
+        # Xtilde_frames = []
         for k, R_frame in enumerate(R_frames):
-            Xtilde_frames[k]=R_frame + Xtilde_frames[k]
+            Xtilde_frames[k] = Xtilde_frames[k] + R_frame
         ###################################################
-        # 5. Solve (Xtilde+R) * alpha = X for unknown alpha
-        #  + the classical method (Reiss2018) uses the 
+        # b) Solve (Xtilde+R) * alpha = X for unknown alpha
+        #  + the classical method (Reiss2018) uses the
         #    least square approach
         # TODO Implement the other methods as well
+        # TODO why is it not converging faster???
         ###################################################
-        minimize(Xtilde_frames, X)
-        
+        [alpha, Xhat_coef] = minimize(Xtilde_frames, X)
+        ######################################################
+        # 5. calculate new modes from optimized coefficients
+        ######################################################
+        update_and_reduce_modes(Xtilde_frames, alpha, Xhat_coef, nmodes)
         #############################################
-        # 5. Add up all the frames to compute Xtilde
+        # 6. update approximation:
+        # Add up all the frames to compute Xtilde
         #############################################
         Xtilde *= 0
         for k, Xframe in enumerate(Xtilde_frames):
 
-            Xtilde+=Xframe.shift(Xframe.build_field()[0])
-            
-            # we plot the first 3 frames 
+            Xtilde += Xframe.inv_shift(Xframe.build_field()[0])
+            # we plot the first 3 frames
             if visualize and k <= 3:
-                subplot(1,len(Xtilde_frames)+2,k+3)
+                subplot(1, len(Xtilde_frames)+2, k+3)
                 Xframe.plot_field()
                 plt.clim(clims)
-                plt.title(r"$q^"+str(k)+"(x,t)$")        
-                #plt.xlabel(r"$N_x$")
+                plt.title(r"$q^"+str(k)+"(x,t)$")
+                # plt.xlabel(r"$N_x$")
             plt.pause(0.05)
 
     ###########################################################################
     # End of MAIN LOOP
     ###########################################################################
-   
-    
+    return Xtilde_frames
