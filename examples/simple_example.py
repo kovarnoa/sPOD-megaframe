b259135167ecd8c3c660fe19e7000f3333978369
Philipp137 <philipp.l.krah@gmail.com>
2018-12-04 11:28:30 +0100

    added rel_err to spod output

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -10,53 +10,54 @@ Created on Fri Nov 30 09:11:12 2018
 # IMPORTED MODULES
 ###############################################################################
 import numpy as np
-from numpy import exp, mod
+from numpy import exp, mod,meshgrid
 import matplotlib.pyplot as plt
-from sPOD_tools import frame,sPOD
+from sPOD_tools import frame, sPOD
 ###############################################################################
 
 ##########################################
 #%% Define your DATA:
 ##########################################
 plt.close("all")
-Nx = 200 # number of grid points in x 
-Nt = 250 # numer of time intervalls
-dt = 0.01 # size of time intervall
-T = Nt*dt # total time
+Nx = 200  # number of grid points in x
+Nt = 250  # numer of time intervalls
+dt = 0.01  # size of time intervall
+T = Nt*dt  # total time
 L = 2*np.pi  # total domain size
-x0 = L*0.5 # starting point of the gauss puls
-sigma=L/50 # standard diviation of the puls
+x0 = L*0.5  # starting point of the gauss puls
+sigma = L/50  # standard diviation of the puls
 
-nmodes = 1 # reduction of singular values
-x = np.linspace(0,L,Nx)
-t = np.linspace(0,T,Nt)
-dx=x[1]-x[0]
-dt=t[1]-t[0]
-c=dx/dt
-[T,X] =meshgrid(t,x)
-fun = lambda x,t: 0.5 * exp( -(mod((x-c*t),L)-x0)**2/sigma**2 ) + \
-                  0.5 * exp( -(mod((x+c*t),L)-x0)**2/sigma**2 )
+nmodes = 1  # reduction of singular values
+x = np.linspace(0, L, Nx)
+t = np.linspace(0, T, Nt)
+dx = x[1]-x[0]
+dt = t[1]-t[0]
+c = dx/dt
+[T, X] = meshgrid(t, x)
+fun = lambda x, t: 0.5 * exp(-(mod((x-c*t), L)-x0)**2/sigma**2) + \
+                   0.5 * exp(-(mod((x+c*t), L)-x0)**2/sigma**2)
 
 # Define your field as a list of fields:
-# For example the first element in the list can be the density of 
+# For example the first element in the list can be the density of
 # a flow quantity and the second element could be the velocity in 1D
 density = fun(X, T)
 velocity = fun(X, T)
-fields = [density]#, velocity]
+fields = [density]  #, velocity]
 
 
 #######################################
-#%% CALL THE SPOD algorithm
+# %% CALL THE SPOD algorithm
 ######################################
-sPOD_frames = sPOD(fields, [-c,c], dx, dt, nmodes=1, eps=1e-4, Niter=30, visualize=False)
+sPOD_frames, rel_err = sPOD(fields, [-c, c], dx, dt, \
+                            nmodes=1, eps=1e-16, Niter=80, visualize=False)
 
-#####################################
-#%% visualize your results:
-#####################################
+###########################################
+# %% 1. visualize your results: sPOD frames
+##########################################
 # the result is a list of the decomposed field.
 # each element of the list contains a frame of the decomposition.
 # If you want to plot the k-th frame use: 
-## 1. frame
+# 1. frame
 k_frame = 0
 plt.suptitle("sPOD Frames")
 plt.subplot(121)
@@ -64,14 +65,22 @@ sPOD_frames[k_frame].plot_field()
 plt.xlabel(r'$N_x$')
 plt.ylabel(r'$N_t$')
 plt.title(r"$q^"+str(k_frame)+"(x,t)$")
-## 2. frame
+# 2. frame
 k_frame = 1
 plt.subplot(122)
 sPOD_frames[k_frame].plot_field()
 plt.xlabel(r'$N_x$')
 plt.ylabel(r'$N_t$')
 plt.title(r"$q^"+str(k_frame)+"(x,t)$")
-# by default this will plot the field in the first component 
+# by default this will plot the field in the first component
 # of your field list (here: density)
 
- 
+###########################################
+# 2. visualize your results: relative error
+##########################################
+
+plt.figure()
+plt.semilogy(rel_err)
+plt.title("relative error")
+plt.ylabel(r"$\frac{||X - \tilde{X}_i||_2}{||X||_2}$")
+plt.xlabel(r"$i$")
--- lib/sPOD_tools.py
+++ lib/sPOD_tools.py
@@ -305,7 +305,7 @@ def sPOD(X, velocities, dx, dt, nmodes=2, eps=1e-4, Niter=5, visualize=True):
     Xtilde_frames = [frame(v, dx, dt, Xtilde, nmodes) for v in velocities]
     rel_err = 1
     it = 0
-
+    results = {"rel_err": []} # save all the output results in the dict
     ###########################################################################
     # MAIN LOOP
     ###########################################################################
@@ -320,7 +320,7 @@ def sPOD(X, velocities, dx, dt, nmodes=2, eps=1e-4, Niter=5, visualize=True):
         ###############################
         R = X - Xtilde
         rel_err = norm(R)/norm(X)  # relative error
-
+        results["rel_err"].append(rel_err)
         ##########################################
         print("iter= ", it, "rel err= ", rel_err)
         ##########################################
@@ -380,4 +380,4 @@ def sPOD(X, velocities, dx, dt, nmodes=2, eps=1e-4, Niter=5, visualize=True):
     ###########################################################################
     # End of MAIN LOOP
     ###########################################################################
-    return Xtilde_frames
+    return Xtilde_frames, results.get('rel_err')
