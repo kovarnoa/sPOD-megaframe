3a61964d46067cdc9f604c4891f5d50fb33cb29a
Philipp Krah <philipp.l.krah@gmail.com>
2021-09-08 15:31:50 +0200

    examples

--- examples/07_test_case_wings.py
+++ examples/07_test_case_wings.py
@@ -24,9 +24,8 @@ cm = farge_colormap_multi()
 #%% Define your DATA:
 ##########################################
 plt.close("all")
-data = loadmat('ALL.mat')
-data = data['data']
-data = data[:,:,352:1056,0:704]
+data = loadmat('a30b10/ALL.mat')
+data = data['data'][:,:,::4,::4]
 
 Ngrid = [data.shape[2], data.shape[3]]  # number of grid points in x and y
 Nt = data.shape[1]                      # Number of time intervalls
@@ -47,34 +46,47 @@ c = dx/dt
 [Y,X] = meshgrid(y,x)
 
 rotation1   = np.zeros([Nt])
-rotation1   = pi/6 * np.cos(2*pi*time) + pi/3 * np.sin(2*pi*time)    # frame 1
+rotation1   = pi/6 * np.cos(2*pi*time) + pi/18 * np.sin(2*pi*time)    # frame 1
 shift1      = np.zeros([2,Nt])
+shift2      = np.zeros([2,Nt])
 rotation2   = np.zeros([Nt])
-rotation2   = pi/6 * np.cos(2*pi*time) + pi/3 * np.sin(2*pi*time)   # frame 2
-rotation3   = np.zeros([Nt])
+rotation2   = pi/6 * np.cos(2*pi*time) + pi/18 * np.sin(2*pi*time)   # frame 2
 
 q = np.zeros(data_shape)
 #for nvar in range(Nvar):
 for it,t in enumerate(time):
     q[:,:,0,it] = np.array(data[0,it,:,:]).T
-    
-    shift1[0,it] = 0
-    shift1[1,it] = 0.5 * np.cos(2*pi*t)
-   
+    plt.pause(0.01)
+    shift1[0,it] = 0.3*L[0]
+    shift1[1,it] = -0.025 * np.cos(2*pi*t)
+    shift2[0,it] = 0.2*L[0]
+    shift2[1,it] = 0
+
 
 # %% Create Trafo
 #trafo_1 = transforms(data_shape,L, shifts = shift1, dx = [dx2,dy2], use_scipy_transform=False )
-trafo_1 = transforms(data_shape,L, trafo_type="shiftRot", shifts = shift1, dx = [dx,dy], rotations = rotation1, rotation_center=[0.4*L[0],0.5*L[1]], use_scipy_transform=False)
-trafo_2 = transforms(data_shape,L, trafo_type="rotation", dx = [dx,dy], rotations = rotation2, rotation_center=[0.6*L[0],0.5*L[1]], use_scipy_transform=False )
+trafo_1 = transforms(data_shape,L, trafo_type="shiftRot", shifts = shift1, dx = [dx,dy], rotations = rotation1,  use_scipy_transform=True)
+trafo_2 = transforms(data_shape,L, trafo_type="shiftRot", shifts = shift2, dx = [dx,dy], rotations = rotation2,  use_scipy_transform=True)
+#trafo_2 = transforms(data_shape,L, trafo_type="rotation", dx = [dx,dy], rotations = rotation2, rotation_center=[0.6*L[0],0.5*L[1]], use_scipy_transform=False )
 
-qshift1 = trafo_1.reverse(q)
+qshift1 = trafo_2.reverse(q)
 qshift2 = trafo_2.reverse(q)
+for it in range(0,len(time),5):
+    plt.pcolormesh(X,Y,qshift1[...,0,it])
+    plt.pause(0.001)
 qshiftreverse = trafo_1.apply(trafo_1.reverse(q))
 res = q-qshiftreverse
+
+for it in range(0,len(time),5):
+    plt.pcolormesh(X,Y,res[...,0,it])
+    plt.pause(0.001)
 err = np.linalg.norm(np.reshape(res,-1))/np.linalg.norm(np.reshape(q,-1))
+err_hist = [np.linalg.norm(np.reshape(res[...,it],-1))/np.linalg.norm(np.reshape(q[...,it],-1)) for it in range(len(time))]
 print("err =  %4.4e "% err)
-plt.pcolormesh(X,Y,q[...,0,0]-qshiftreverse[...,0,0],cmap = cm)
+#plt.pcolormesh(X,Y,q[...,0,0]-qshiftreverse[...,0,0],cmap = cm)
 plt.colorbar()
+
+
 # %% Test Trafo
 
 # figs,axs = plt.subplots(3,1,sharex=True)
@@ -90,13 +102,14 @@ plt.colorbar()
 #     axs[2].pcolormesh(X,Y,q[...,0,it]-qshiftreverse[...,0,it])
 #     plt.show()
 #     plt.pause(0.001)
-    
+
 # %% Run shifted POD
 transforms = [trafo_1, trafo_2]
 qmat = np.reshape(q,[-1,Nt])
 #qframes, q = shifted_POD(q, transforms, nmodes=2, eps=1e-4, Niter=20, visualize=True)
-mu = np.prod(np.size(qmat)) / (4 * np.sum(np.abs(qmat)))*0.1
+mu = np.prod(np.size(qmat)) / (4 * np.sum(np.abs(qmat)))*0.01
 ret = shifted_rPCA(qmat, transforms, eps=1e-4, Niter=50, visualize=True, mu = mu)
 qframes, qtilde = ret.frames, ret.data_approx
 qtilde = np.reshape(qtilde,data_shape)
 plt.pcolormesh(X,Y,q[...,0,10]-qtilde[...,0,10])
+
